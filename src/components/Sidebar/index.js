be686dae21f8e1198dcab425977e1c93536ba553
dragfire <asem.devajit@gmail.com>
2018-01-14 17:54:24 +0530

    Add mobile detection

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import cssModules from 'react-css-modules';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import styles from './sidebar.scss';
 
 class Sidebar extends Component {
   static propTypes = {
+    isMobileDevice: PropTypes.bool.isRequired,
   };
 
   static defaultProps = {
@@ -17,8 +19,11 @@ class Sidebar extends Component {
   }
 
   render() {
+    const { isMobileDevice } = this.props;
     return (
-      <div styleName="Sidebar">
+      <div styleName={classNames("Sidebar", {
+        "Sidebar--is-mobile": isMobileDevice,
+      })}>
         <h2>Sidebar</h2>
       </div>
     );
