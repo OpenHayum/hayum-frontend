c401a8931d68a9cc87ac522a32790ae08785e718
dragfire <asem.devajit@gmail.com>
2018-01-26 09:32:58 +0530

    Change PORT

--- server/index.js
+++ server/index.js
@@ -1,55 +1,55 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import path from 'path';
-import debugLog from 'debug';
-import controllers from './controllers';
-import morgan from 'morgan';
-import mongooseStart from './config/mongo.config';
-import { port } from './config/hayum.config';
-import { notFound } from './errors';
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import debugLog from "debug";
+import controllers from "./controllers";
+import morgan from "morgan";
+import mongooseStart from "./config/mongo.config";
+import { port } from "./config/hayum.config";
+import { notFound } from "./errors";
 
-const debug = debugLog('HAYUM: server');
+const debug = debugLog("HAYUM: server");
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-app.use('/api', controllers);
+app.use("/api", controllers);
 
-app.use('/', (req, res) => {
-  const filePath = path.resolve(__dirname, '..', 'build', 'index.html');
-  res.sendFile(filePath)
+app.use("/", (req, res) => {
+  const filePath = path.resolve(__dirname, "..", "build", "index.html");
+  res.sendFile(filePath);
 });
 
 app.use((req, res, next) => {
   next(notFound());
 });
 
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use((err, req, res) => {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
-      error: err,
+      error: err
     });
   });
 }
 
 app.use((err, req, res) => {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
-    error: {},
+    error: {}
   });
 });
 
-app.listen(port, (error) => {
+app.listen(process.env.PORT || port, error => {
   mongooseStart();
   if (error) {
-    debug('error', error);
+    debug("error", error);
   }
-  debug('Listening: ', port);
+  debug("Listening: ", port);
 });
