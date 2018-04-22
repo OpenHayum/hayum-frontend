8d3b3b9cbd6caa3e12cb8cb10db1299a9c8727a8
dragfire <asem.devajit@gmail.com>
2018-04-22 17:23:33 +0530

    Update FileUpload propTypes

--- src/components/User/TrackUploader/FileUpload.js
+++ src/components/User/TrackUploader/FileUpload.js
@@ -6,11 +6,17 @@ import styles from './fileUpload.scss';
 import { FilePond, File } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 
-const FileUpload = ({ label, files }) => {
+const FileUpload = ({
+  label,
+  maxFiles,
+  allowMultiple,
+  files,
+  apirUrl,
+}) => {
   return (
     <div styleName="FileUpload">
-      <h5>UPLOAD AUDIO THUMBNAIL:</h5>
-      <FilePond allowMultiple={true} maxFiles={1} server="/api">
+      <h5>{label}</h5>
+      <FilePond allowMultiple={allowMultiple} maxFiles={maxFiles} server={apirUrl}>
         {files.map(file => (
           <File key={file} source={file} />
         ))}
@@ -21,12 +27,16 @@ const FileUpload = ({ label, files }) => {
 
 FileUpload.propTypes = {
   label: PropTypes.string.isRequired,
+  apirUrl: PropTypes.string,
+  allowMultiple: PropTypes.bool,
   maxFiles: PropTypes.number,
   files: PropTypes.array,
 };
 
 FileUpload.defaultProps = {
   maxFiles: 1,
+  apirUrl: "/api",
+  allowMultiple: false,
   files: [],
 };
 
