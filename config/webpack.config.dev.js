6e7c5652a2c6133718f4fb9fb6787a28b56f1fc6
dragfire <asem.devajit@gmail.com>
2019-05-30 13:39:56 -0400

    Add babelrc

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -138,30 +138,10 @@ module.exports = {
                         test: /\.[tj]sx?$/,
                         include: paths.appSrc,
                         use: [
-
-
                             {
                                 loader: 'awesome-typescript-loader',
                                 options: {
                                     cacheDirectory: true,
-                                    useBabel: true,
-                                    babelOptions: {
-                                        babelrc: false,
-                                        presets:[],
-                                        plugins: [
-                                            [
-                                                'react-css-modules',
-                                                {
-                                                    "filetypes": {
-                                                        ".scss": {
-                                                            "syntax": "postcss-scss"
-                                                        }
-                                                    },
-                                                    "generateScopedName": "[name]__[local]--[hash:base64:8]"
-                                                }
-                                            ],
-                                        ]
-                                    }
                                 },
 
                             },
